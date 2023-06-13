e8165dd96e7f096e0b3b302b0eb74697aa85d558
sajadsafary <alosadjad@gmail.com>
2023-06-13 10:39:29 +0330

    registration User and Profiel models to admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import User, Profile
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+
+class ProfileInline(admin.TabularInline):
+    model = Profile
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'is_staff', )
+
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups',)
+    search_fields = ('username', 'email', 'first_name', 'last_name', )
+    ordering = ('last_name', 'first_name', )
+
+    inlines = [ProfileInline, ]
+
